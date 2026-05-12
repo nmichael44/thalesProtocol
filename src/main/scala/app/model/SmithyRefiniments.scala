02156658bb7e90c844ea9f6aec885f6682766342
neophytos-m <neophytosm@easymarkets.com>
2026-05-12 11:49:32 +0300

    Simplify given implementation for RefinementProvider[NonEmptyVecSmithy, List[A], NonEmptyVector[A]].

--- src/main/scala/app/model/SmithyRefiniments.scala
+++ src/main/scala/app/model/SmithyRefiniments.scala
@@ -7,14 +7,11 @@ import java.time.Instant
 import smithy4s.{Refinement, RefinementProvider}
 import smithy4s.time.Timestamp
 
-private val anyVectorRefinement = Refinement.drivenBy[NonEmptyVecSmithy](
-  (list: List[Any]) => NonEmptyVector.fromVector(list.toVector).toRight("List cannot be empty"),
-  (nev: NonEmptyVector[Any]) => nev.toVector.toList,
-)
-
-private type RefinementProvType[A] = RefinementProvider[NonEmptyVecSmithy, List[A], NonEmptyVector[A]]
-
-given [A]: RefinementProvType[A] = anyVectorRefinement.asInstanceOf[RefinementProvType[A]]
+given [A]: RefinementProvider[NonEmptyVecSmithy, List[A], NonEmptyVector[A]] =
+  Refinement.drivenBy[NonEmptyVecSmithy](
+    (list: List[A]) => NonEmptyVector.fromVector(list.toVector).toRight("List cannot be empty"),
+    (nev: NonEmptyVector[A]) => nev.toVector.toList,
+  )
 
 given RefinementProvider[JavaInstantRefinement, Timestamp, Instant] =
   Refinement.drivenBy[JavaInstantRefinement](
